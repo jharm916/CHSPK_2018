a5e253464c28e3793bf4927ecb4964dda97a4e55
Jeremy <Jeremy@hbinnovations.net>
2019-01-06 02:32:39 -0500

    adding new update

---
+++ update2k19.php
@@ -0,0 +1,195 @@
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+	<head>
+		<title>Chesapeak Music</title>
+		
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+		<meta title ="Chesapeak Music">
+		<meta charset="UTF-8">
+		<meta name="description" content="Chesapeak Music | Inspiration | Good Music | Lifestyle | chspk | collaborate & support | DMV">
+		<meta name="keywords" content="CHSPK,chesapeak,bayhouse,chesapeake,music,rap,rapping,rappers,hiphop,hip-hop, Baltimore,Maryland,dc,productions,produce,producer,city,urban,appeal,clothing,style,art,artists,shirt,shirts, photography,dmv,eskettit,rich,cash,money,recording,md,flow,sale,business,enterprise,fresh,shots,flicks,flick,video,videos,recording,coast,savethebay,save,the,bay,promotion,promotional,squad,content,knowledge,mass,appeal,restoration,restore,water,ocean,clean,recycle,energy,renewable,resources,resource,source,out, restorative,newage,positive,vibes,new-age, revolutionary,ideas,higher,level,concept, capture,milleneal,generation,jam,inspire,raw,energy,emotion,depth,lyricism,lyrics,hard,go,smooth,dope,
+			Chesapeak,East,Coast,hiphop,hip,hop,music,PorterBay,porter,bay,rap,art,vinci,scrolling,twitter">
+
+		<meta name="Resource-type" content="Document" />
+		<link rel="icon" type="image/png" href="images/favicon-32x32.png" sizes="32x32" />
+		<link rel="icon" type="image/png" href="images/favicon-16x16.png" sizes="16x16" />
+		<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
+	
+		<!--style-->
+		<link type="text/css" rel="stylesheet" href="style/css/lightgallery.css" />
+		<link href="style/css/style.css" rel="stylesheet">
+		<link href="style/css/parallax.css" rel="stylesheet">
+		
+		<style>
+		
+			h1{
+				font-size: 5em;
+				font-family: arial,helvetica;
+				color: #fff;
+				margin:0;
+				padding:0;
+			}
+
+			.section{
+				text-align:center;
+				overflow: hidden;
+			}
+
+			#section0 .layer{
+				position: absolute;
+				z-index: 4;
+				width: 100%;
+				left: 0;
+				top: 0;
+				-webkit-transform: translate3d(0,0,0);
+				-ms-transform: translate3d(0,0,0);
+				transform: translate3d(0,0,0);
+			}
+			
+			.layerleft{
+				position: fixed;
+				width: 15%;
+				left: 0;
+				top: 30%;
+			}
+			
+			.layermiddle{
+				position: fixed;
+				width: 70%;
+				left: 15%;
+				top: 12%;
+			}
+			
+			.layerright{
+				position: fixed;
+				width: 15%;
+				right: 0;
+				top: 30%;
+			}
+
+
+			#section0{
+				overflow: hidden;
+			}
+			
+			/* Backgrounds will cover all the section
+			* --------------------------------------- */
+			#section1,
+			#section2,
+			#section3,
+			#slide1, #slide2, #slide3, #slide4, #slide5, #slide6, #slide7, #slide8, #slide9{
+				-webkit-background-size: cover;
+				-moz-background-size: cover;
+				-o-background-size: cover;
+				background-size: cover;
+				background-size: cover;
+				background-attachment: absolute;
+				width: 100%;
+				left: 0;
+				top: 0;
+				-webkit-transform: translate3d(0,0,0);
+				-ms-transform: translate3d(0,0,0);
+				transform: translate3d(0,0,0);
+				background-repeat: no-repeat;
+				 no-repeat fixed;
+				-webkit-background-size: cover;
+				-moz-background-size: cover;
+				-o-background-size: cover;
+				background-size: cover;
+
+			}
+
+			/* Defining each section background and styles
+			* --------------------------------------- */
+
+			#section1{
+				background-image: url(http://chesapeakmusic.com/video/ezgif.com-optimize.gif);
+				padding: auto;
+				-webkit-background-size: cover;
+				-moz-background-size: cover;
+				-o-background-size: cover;
+				background-size: cover;
+				background-position: center;		
+			}
+
+			#section2{
+				background-image: url(images/content/freee.png);
+				padding: auto;
+				-webkit-background-size: cover;
+				-moz-background-size: cover;
+				-o-background-size: cover;
+				background-size: cover;
+				background-position: center;
+			}
+
+			/*Adding background for the slides
+			* --------------------------------------- */
+			#slide1{
+				background-image: url(images/content/gallery1.jpg);
+				padding: auto;
+			}
+			#slide2{
+				background-image: url(images/content/gallery2.png);
+				padding: auto;
+			}
+			#slide3{
+				background-image: url(images/content/gallery3.jpg);
+				padding: auto;
+			}
+			#slide4{
+				background-image: url(images/content/gallery4.jpeg);
+				padding: auto;
+			}
+			#slide5{
+				background-image: url(images/content/gallery5.jpeg);
+				padding: auto;
+			}
+			#slide6{
+				background-image: url(images/content/gallery6.png);
+				padding: auto;
+			}
+
+
+			/* Bottom menu
+			* --------------------------------------- */
+
+			#infoMenu li a {
+				color: #fff;
+			}
+			
+		</style>
+	</head>
+	
+	<body class="is-preload" style="zoom: 1;">
+		<div class = "section" id="section0">
+		
+			<p>Middle Section Center</p>
+			
+			<ul id="aniimated-thumbnials" style="overflow: scroll; height: 100%; width: 100%;">
+				<li data-src="images/chspk_popout/DSC2.JPG">
+					<img src="images/litty3.png" />
+				</li>
+				<li data-src="images/chspk_popout/DSC2.JPG">
+					<img src="images/chspk_popout/DSC2.JPG" />
+				</li>
+			</ul>
+			
+			
+			<div id="background">
+				<div class="star" data-parallaxify-range="300" />
+				
+				<div class="layer">
+					<p>Left Section Center</p>
+					
+					
+					
+					<div id="wrapper">
+					
+					</div>
+				</div> <!--layer-->
+				
+			</div> <!--background-->
+		</div>
+	</body>
+	
+</html>
